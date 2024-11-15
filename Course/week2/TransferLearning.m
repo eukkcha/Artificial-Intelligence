a5877f5bf9afd5febe770e6cf8d2466057622ae6
JeonYuchan <eukkchan03@gmail.com>
2024-11-15 10:12:25 +0900

    Update TransferLearning.m

--- Course/week2/TransferLearning.m
+++ Course/week2/TransferLearning.m
@@ -1,1 +1,8 @@
 [net, classes] = imagePretrainedNetwork("alexnet");
+
+% View the layers of AlexNet and save the layers to a variable
+layers = net.Layers
+
+% Save the first and last layers of AlexNet
+layers_input = layers(1)
+layers_output = layers(end)
